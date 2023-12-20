55b77ae65f815dd5ce674b6eeee21e1f403962ec
Andreea Dima <andreeaadima@gmail.com>
2023-12-20 13:59:59 +0200

    Add docker comments + mysql playground ADDENDUM

--- src/main/resources/mysql-playground/playground.sql
+++ src/main/resources/mysql-playground/playground.sql
@@ -2,7 +2,7 @@
 USE campaign_manager_db;
 
 -- create the users table
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS user (
     id            VARCHAR(60)     PRIMARY KEY,
     email         VARCHAR(60)     NOT NULL,
     gender        VARCHAR(60)     NOT NULL,
@@ -15,13 +15,13 @@ CREATE TABLE IF NOT EXISTS users (
 SHOW TABLES;
 
 -- add a unique constraint on the email column
-ALTER TABLE users ADD CONSTRAINT email_unique UNIQUE (email);
+ALTER TABLE user ADD CONSTRAINT email_unique UNIQUE (email);
 
 -- insert some data into the users table
-INSERT INTO users (id, email, gender, age, interests, created_date)
+INSERT INTO user (id, email, gender, age, interests, created_date)
 values
 ('aaa', 'ana@adobe.com',"FEMALE",20,'FASHION', '2013-08-05 18:19:03'),
 ('bbb', 'ionut@adobe.com',"MALE",23,'SPORTS', '2013-08-05 18:19:03');
 
 -- display the data in the users table
-SELECT * from users;
+SELECT * from user;
